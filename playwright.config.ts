20bc8ee3dd9cc902cc3e0d3356ea1b55a29180c1
Albert Sodel <sodelalbert@gmail.com>
2025-12-04 10:26:12 +0100

    Set headless mode in Playwright configuration based on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     screenshot: "only-on-failure",
 
     /* Run tests in headed mode */
-    headless: false,
+    headless: process.env.CI ? true : false,
   },
 
   /* Configure projects for major browsers */
